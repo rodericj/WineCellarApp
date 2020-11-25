2a515c15322bd3882aec1c96cd7f015c6766c40c
Roderic Campbell <roderic@gmail.com>
2020-11-25 09:48:41 -0800

    Add some spacing around the text content
    Breathing room

--- WineCellarApp/Views/BottleDetail.swift
+++ WineCellarApp/Views/BottleDetail.swift
@@ -21,8 +21,7 @@ struct BottleDetail: View {
     var body: some View {
         VStack (alignment: .center) {
             Map(coordinateRegion: $region)
-            BottleTextContent(bottle: bottle)
-            Spacer()
+            BottleTextContent(bottle: bottle).padding()
         }.onAppear(perform: {
            performSearch()
         })
@@ -46,7 +45,7 @@ extension BottleDetail {
 }
 
 struct BottleDetail_Previews: PreviewProvider {
-    static let first = Bottle(wineID: "123", title: "Fancy Bottle 1", location: "The cellar", price: 123.4, vintage: "2018", quantity: 1, wineBarcode: "101010", size: "750ml", valuation: 123.4, currency: "USD", locale: "what is locale", country: "France", region: "Burgundy", subRegion: "Fancy Burgundy", appellation: "fanciest appellation", producer: "Jean Luke", sortProducer: "Producer", type: .red, varietal: "Pinot Noir", masterVarietal: "Master Pinot Noir", designation: "Designation", vineyard: "Left Vineyard")
+    static let first = Bottle(wineID: "123", title: "Domain Sylvain Langoureau Saint Aubin 1er Cru En Remilly", location: "The cellar", price: 123.4, vintage: "2018", quantity: 1, wineBarcode: "101010", size: "750ml", valuation: 123.4, currency: "USD", locale: "what is locale", country: "France", region: "Burgundy", subRegion: "Fancy Burgundy", appellation: "fanciest appellation", producer: "Jean Luke", sortProducer: "Producer", type: .red, varietal: "Pinot Noir", masterVarietal: "Master Pinot Noir", designation: "Designation", vineyard: "Left Vineyard")
 
     static var previews: some View {
         BottleDetail(bottle: first)
