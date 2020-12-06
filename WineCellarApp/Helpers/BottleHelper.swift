35334f8db45152b0ec4a48da0b78716cf66aafbc
Roderic Campbell <roderic@gmail.com>
2020-12-05 19:54:28 -0800

    Add a bottle helper
    Uses a bottle as a bridge between WineCellar and WineRegionLib

---
+++ WineCellarApp/Helpers/BottleHelper.swift
@@ -0,0 +1,75 @@
+//
+//  BottleHelper.swift
+//  WineCellarApp
+//
+//  Created by Roderic Campbell on 12/4/20.
+//
+
+import WineCellar
+import WineRegionLib
+
+// For search functionality
+extension Bottle {
+    func filter(on searchText: String) -> Bool {
+        appellation.contains(searchText) ||
+            country.contains(searchText) ||
+            description.contains(searchText) ||
+            designation.contains(searchText) ||
+            locale.contains(searchText) ||
+            location.contains(searchText) ||
+            masterVarietal.contains(searchText) ||
+            String(price).contains(searchText) ||
+            producer.contains(searchText) ||
+            region.contains(searchText) ||
+            sortProducer.contains(searchText) ||
+            subRegion.contains(searchText) ||
+            title.contains(searchText) ||
+            type.rawValue.contains(searchText) ||
+            varietal.contains(searchText) ||
+            vineyard.contains(searchText) ||
+            vintage.contains(searchText)
+    }
+}
+
+// For marrying our nav information with the bottle object
+extension Bottle {
+
+    private func usaRegion() -> AppelationDescribable? {
+        switch region {
+        case WineCountry.USA.California.title:
+            let app =  WineCountry.USA.California.Appelation(rawValue: appellation)
+            if app == nil {
+                print("No mapping for CA appellation \(appellation)")
+            }
+            return app
+        default:
+            return nil
+        }
+    }
+    private func frenchRegion() -> AppelationDescribable? {
+        switch region {
+        case WineCountry.France.Bordeaux.title:
+            let bordeaux = WineCountry.France.Bordeaux.Medoc.Appelation(rawValue: appellation)
+            print(appellation)
+            assert(bordeaux != nil, "must define \(appellation)")
+            return bordeaux
+        case WineCountry.France.Burgundy.title:
+            let burgundy = WineCountry.France.Burgundy.Appelation(rawValue: appellation)
+            print(appellation)
+            assert(burgundy != nil, "must define \(appellation)")
+            return burgundy
+        default:
+            return nil
+        }
+    }
+    var libAppelation: AppelationDescribable? {
+        switch country {
+        case WineCountry.France.title:
+            return frenchRegion()
+        case WineCountry.USA.title:
+            return usaRegion()
+        default:
+            return nil
+        }
+    }
+}
