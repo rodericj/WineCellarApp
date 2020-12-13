0431897c9008d9862c1a6863519d6a0795a89655
Roderic Campbell <roderic@gmail.com>
2020-12-12 22:23:15 -0800

    Fix the issue where multiPolygons were not loading

--- WineCellarApp/Views/WineMapView.swift
+++ WineCellarApp/Views/WineMapView.swift
@@ -20,24 +20,41 @@ class WineMapView: MKMapView {
     
     public func add(features: [MapKitOverlayable]) {
         print("Features count: \(features.count)")
-        features.compactMap { $0 as? MKGeoJSONFeature }
+        features
+            .compactMap { $0 as? MKPolygon }
+            .forEach { polygon in
+                print("we got some polygons")
+                self.addOverlay(polygon)
+            }
+        features
+            .compactMap { $0 as? MKMultiPolygon }
+            .forEach { multiPolygon in
+                print("we got some multi polygons")
+                multiPolygon.polygons.forEach {
+                    debugPrint("polygon in multipolygon \($0)")
+                    self.addOverlay($0)
+                }
+            }
+        features
+            .compactMap { $0 as? MKGeoJSONFeature }
             .forEach { feature in
             feature.geometry
                 .map { $0 as? MKPolygon  }
                 .compactMap { $0 }
-                .forEach { multiPolygon in
-//                    debugPrint("polygon")
-                    self.addOverlay(multiPolygon)
+                .forEach { polygon in
+                    debugPrint("polygon")
+                    self.addOverlay(polygon)
                 }
 
 //            debugPrint("The feature \(feature.geometry)")
-            feature.geometry
+            feature
+                .geometry
                 .map { $0 as? MKMultiPolygon }
                 .compactMap { $0 }
                 .forEach { multiPolygon in
-//                    debugPrint("polygon")
+                    debugPrint("multiPolygon")
                     multiPolygon.polygons.forEach {
-//                        debugPrint("polygon \($0)")
+                        debugPrint("polygon in multipolygon \($0)")
                         self.addOverlay($0)
                     }
                 }
