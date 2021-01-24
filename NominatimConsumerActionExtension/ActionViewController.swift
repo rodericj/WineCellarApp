d4586cb847e82a2ac75d890ba5f40d6c7ff98058
Roderic Campbell <roderic@gmail.com>
2021-01-23 23:12:08 -0800

    Cleanup the template extension code because it was a mess

--- NominatimConsumerActionExtension/ActionViewController.swift
+++ NominatimConsumerActionExtension/ActionViewController.swift
@@ -9,6 +9,13 @@ import UIKit
 import MobileCoreServices
 import WineRegionLib
 import MapKit
+
+enum ActionError: Error {
+    case invalidDataFormat
+    case missingTitle
+    case invalidGeoJson
+}
+
 class ActionViewController: UIViewController {
 
     var newRegionData: NewRegionData? = nil
@@ -24,59 +31,59 @@ class ActionViewController: UIViewController {
         super.viewDidLoad()
     
         // Get the item[s] we're handling from the extension context.
-        
-        // TODO clean this all up, it is a mess
-        for item in self.extensionContext!.inputItems as! [NSExtensionItem] { // TODO get rid of the !
-            print(item)
-            for provider in item.attachments! { // TODO get rid of the !
-                print(provider)
-                if provider.hasItemConformingToTypeIdentifier(kUTTypeURL as String) {
-                    weak var weakMapView = self.mapView
-                    provider.loadItem(forTypeIdentifier: kUTTypeURL as String, options: nil, completionHandler: { [weak self] (contentURL, error) in
-                        let decoder = JSONDecoder()
-                        decoder.keyDecodingStrategy = .convertFromSnakeCase
-                        guard let url = contentURL as? URL else {
-                            return
-                        }
-                        do {
-                            let data = try Data(contentsOf: url)
-                            let jsonResponse = try JSONSerialization.jsonObject(with: data, options: []) as? [String: AnyObject]
-                            let name = jsonResponse!["localname"] as? String
-                            let data1 =  try JSONSerialization.data(withJSONObject: jsonResponse!["geometry"]!, options: JSONSerialization.WritingOptions.prettyPrinted) // first of all convert json to the data
-                            self?.newRegionData = NewRegionData(title: name ?? "Unknown", geoJson: data1)
-                            let convertedString = String(data: data1, encoding: String.Encoding.utf8) // the data will be converted to the string
-                            print(convertedString ?? "defaultvalue")
+                
+        extensionContext?.inputItems
+            .compactMap { $0 as? NSExtensionItem }
+            .map { $0.attachments }
+            .compactMap { $0 }
+            .forEach{ providerArray in
+                providerArray
+                    .filter { $0.hasItemConformingToTypeIdentifier(kUTTypeURL as String) }
+                    .forEach { provider in
+                        provider.loadItem(forTypeIdentifier: kUTTypeURL as String, options: nil, completionHandler: { [weak self] (contentURL, error) in
+                            weak var weakMapView = self?.mapView
                             
-                            let geoJsonDecoder = MKGeoJSONDecoder()
-                            let mapObjects = try? geoJsonDecoder.decode(data1)
-
-                            OperationQueue.main.addOperation { 
-                                if let strongMapView = weakMapView, let mapObjects = mapObjects {
-                                    strongMapView.add(features: mapObjects.compactMap { $0 as? MapKitOverlayable })
-                                    
-                                    var mapRect: MKMapRect = .null
-                                    strongMapView.overlays.forEach { overlay in
-                                        mapRect = mapRect.union(overlay.boundingMapRect)
-                                    }
-                                    
-                                    let inset = UIScreen.main.bounds.size.width * 0.05
-                                    let padding =  UIEdgeInsets(top: inset, left: inset, bottom: inset, right: inset)
-                                    
-                                    strongMapView.setVisibleMapRect(mapRect,
-                                                              edgePadding: padding,
-                                                              animated: true)
-                                }
+                            let decoder = JSONDecoder()
+                            decoder.keyDecodingStrategy = .convertFromSnakeCase
+                            guard let url = contentURL as? URL else {
+                                return
                             }
                             
-                        } catch {
-                            print(error)
-                            return
-                        }
-                    })
-                }
+                            do {
+                                let data = try Data(contentsOf: url)
+                                let jsonObject = try JSONSerialization.jsonObject(with: data, options: [])
+                                guard let jsonResponse =  jsonObject as? [String: AnyObject] else {
+                                    throw ActionError.invalidDataFormat
+                                }
+                                guard let name = jsonResponse["localname"] as? String else {
+                                    throw ActionError.missingTitle
+                                }
+                                guard let geometry = jsonResponse["geometry"] else {
+                                    throw ActionError.invalidGeoJson
+                                }
+                                
+                                // first of all convert json to the data
+                                let geoJson =  try JSONSerialization.data(withJSONObject: geometry,
+                                                                          options: JSONSerialization.WritingOptions.prettyPrinted)
+                                self?.newRegionData = NewRegionData(title: name, geoJson: geoJson)
+                                
+                                let geoJsonDecoder = MKGeoJSONDecoder()
+                                let mapObjects = try? geoJsonDecoder.decode(geoJson)
+                                
+                                OperationQueue.main.addOperation {
+                                    if let strongMapView = weakMapView, let mapObjects = mapObjects {
+                                        strongMapView.add(features: mapObjects.compactMap { $0 as? MapKitOverlayable })
+                                        strongMapView.zoomToOverlays()
+                                    }
+                                }
+                                
+                            } catch {
+                                print(error)
+                                return
+                            }
+                        })
+                    }
             }
-
-        }
     }
 
     @IBAction func done() {
@@ -85,6 +92,22 @@ class ActionViewController: UIViewController {
         self.extensionContext!.completeRequest(returningItems: self.extensionContext!.inputItems, completionHandler: nil)
     }
 
+}
+
+extension MKMapView {
+    func zoomToOverlays() {
+        var mapRect: MKMapRect = .null
+        overlays.forEach { overlay in
+            mapRect = mapRect.union(overlay.boundingMapRect)
+        }
+        
+        let inset = UIScreen.main.bounds.size.width * 0.05
+        let padding =  UIEdgeInsets(top: inset, left: inset, bottom: inset, right: inset)
+        
+        setVisibleMapRect(mapRect,
+                          edgePadding: padding,
+                          animated: true)
+    }
 }
 
 extension ActionViewController: MKMapViewDelegate {
